888605b465df4a9190f9085edbc9fb0178c7d729
mcdfinn <finnigan.mcdonough@gmail.com>
2016-08-10 15:49:13 -0400

    Initial Commit
    initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody2D phys;
+	public int forwardSpeed = 10;
+	public int moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		phys = GetComponent<Rigidbody2D> ();
+
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		moveRight ();
+		moveForward ();
+		moveLeft ();
+		moveDown ();
+	}
+		
+	public void moveForward(){
+		if (Input.GetKey (KeyCode.W)) {
+			phys.velocity = Vector2.up * forwardSpeed;
+		}
+	
+		if (Input.GetKeyUp (KeyCode.W)) {
+			phys.velocity = Vector2.zero;
+		}
+	
+	}
+
+
+
+	public void moveRight(){
+		if (Input.GetKey (KeyCode.D)) {
+			phys.velocity = Vector2.right * moveSpeed;
+		}
+		if (Input.GetKeyUp (KeyCode.D)) {
+			phys.velocity = Vector2.zero;
+		}
+	
+	}
+
+
+	public void moveLeft(){
+		if (Input.GetKey (KeyCode.A)) {
+			phys.velocity = Vector2.left * moveSpeed;
+		}
+
+		if (Input.GetKeyUp (KeyCode.A)) {
+			phys.velocity = Vector2.zero;
+		}
+	
+	}
+
+
+	public void moveDown(){
+		if (Input.GetKey (KeyCode.S)) {
+			phys.velocity = Vector2.down * moveSpeed;
+
+
+		}
+	
+		if (Input.GetKeyUp (KeyCode.S)) {
+			phys.velocity = Vector2.zero;
+		}
+	}		
+	
+
+
+
+
+
+
+
+
+}
+
+
+
---
+++ Assets/Shoot.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	
+	public Rigidbody2D phys;
+	public GameObject bullet;
+	public GameObject realBullet;
+	public int bulletSpeed = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		shootBullet ();
+
+	}
+
+	public void shootBullet(){
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			realBullet = Instantiate (bullet);
+			realBullet.transform.position = this.transform.position + new Vector3 (0, 5, 0);
+			phys = realBullet.GetComponent <Rigidbody2D> ();
+			phys.AddForce(new Vector2(0, bulletSpeed), ForceMode2D.Impulse);
+			print (phys.velocity);
+		
+		
+		}
+	}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+ //
